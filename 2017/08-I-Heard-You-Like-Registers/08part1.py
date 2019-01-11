0452328078cd038137de80fe7ae4bdfda0e5ad6f
dcthomson <drew@thomson.ninja>
2019-01-10 23:07:43 -0800

    done

--- 2017/08-I-Heard-You-Like-Registers/08part1.py
+++ 2017/08-I-Heard-You-Like-Registers/08part1.py
@@ -9,9 +9,12 @@ with open(sys.argv[1], 'r') as f:
         regs[line.split()[0]] = 0
         regs[line.split()[4]] = 0
 
+highest = 0
+
 for i in inst:
     (run, cond) = i.split(" if ")
     (reg, op, val) = cond.split()
+    val = int(val)
     dorun = False
     if op == "<":
         if regs[reg] < val:
@@ -34,9 +37,10 @@ for i in inst:
 
     if dorun:
         (reg, incdec, val) = run.split()
+        val = int(val)
         if incdec == "inc":
             regs[reg] += val
         elif incdec == "dec":
             regs[reg] -= val
 
-print(max(regs, key=regs.get))
+print(regs[max(regs, key=regs.get)])
