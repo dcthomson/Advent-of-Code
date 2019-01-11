34fd8d0c7b224f119f022f93396296bdf07b099a
dcthomson <drew@thomson.ninja>
2019-01-11 15:55:56 -0800

    complete 11.2

---
+++ 2017/11-Hed-Ed/11part2.py
@@ -0,0 +1,33 @@
+import sys
+
+x = 0
+y = 0
+z = 0
+largest = 0
+
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        for dir in line.strip().split(","):
+            if dir == 'n':
+                y += 1
+                z -= 1
+            elif dir == 'ne':
+                x += 1
+                z -= 1
+            elif dir == 'se':
+                x += 1
+                y -= 1
+            elif dir == 's':
+                y -= 1
+                z += 1
+            elif dir == 'sw':
+                x -= 1
+                z += 1
+            elif dir == 'nw':
+                x -= 1
+                y += 1
+            coords = [abs(x), abs(y), abs(z)]
+            if max(coords) > largest:
+                largest = max(coords)
+
+print(largest)
