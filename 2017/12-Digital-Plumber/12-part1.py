ebb11e47b62673e9a45baf9d0724a1ed0b013b0f
dcthomson <drew@thomson.ninja>
2019-01-11 20:50:14 -0800

    complete 12.1

--- 2017/12-Digital-Plumber/12-part1.py
+++ 2017/12-Digital-Plumber/12-part1.py
@@ -1,4 +1,21 @@
 import sys
 
+progs = list()
+
+def getPipes(index, progs, linked):
+    for i in progs[index]:
+        i = int(i)
+        if i not in linked:
+            linked[i] = 1
+            linked = getPipes(i, progs, linked)
+    return linked
+
+linked = dict()
+
 with open(sys.argv[1], 'r') as f:
-    
+    for line in f:
+        line = line.strip().split(" <-> ")[1]
+        progs.append(line.split(", "))
+
+linked = getPipes(0, progs, linked)
+print(len(linked))
