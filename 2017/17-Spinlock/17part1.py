e17e9c2ea2514d5d8acddb6b605324eabbfa236b
dcthomson <drew@thomson.ninja>
2019-01-12 23:39:49 -0800

    17.1 complete

--- 2017/17-Spinlock/17part1.py
+++ 2017/17-Spinlock/17part1.py
@@ -1,25 +1,32 @@
 import sys
 
-step = sys.argv[1]
+step = int(sys.argv[1])
 
 # use linked list
 
-def stepforward(index, cb):
-    return cb[index]
+class Node:
+    # don't really need prev
+    # but we'll add it just in case part 2 needs it
+    def __init__(self, num):
+        self.num = num
+        self.next = self
+        self.prev = self
 
-def insertafter(index, val, cb):
-    tmp = cb[index]
-    cb[index] = val
-    cb[val] = tmp
-    return cb
+    def insertafter(self, node):
+        self.prev = node
+        self.next = node.next
+        self.next.prev = self
+        node.next = self
+        return self
 
-cb = dict()
+index = Node(0)
+zero = index
 
-cb[0] = 0
+for i in range(1, 2018):
+    for j in range(0, step):
+        index = index.next
+    n = Node(i)
+    index = n.insertafter(index)
 
-index = 0
-
-for i in range(1, 2107):
-    for j in (0, step):
-        index = stepforward(index, cb)
-    cb = insertafter(index, i, cb)
+index = index.next
+print(index.num)
