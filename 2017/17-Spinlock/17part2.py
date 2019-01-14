54dbcb4fc85cc103b237bdf683d66dfbfa1fdd87
dcthomson <drew@thomson.ninja>
2019-01-14 05:58:01 -0800

    17.2 changed from using class to dictionary, faster but still too slow

--- 2017/17-Spinlock/17part2.py
+++ 2017/17-Spinlock/17part2.py
@@ -4,28 +4,20 @@ step = int(sys.argv[1])
 
 # use linked list
 
-class Node:
-    # don't really need prev
-    # but we'll add it just in case part 2 needs it
-    def __init__(self, num):
-        self.num = num
-        self.next = self
+cb = dict()
 
-    def insertafter(self, node):
-        self.next = node.next
-        node.next = self
-        return self
+cb[0] = 0
 
-index = Node(0)
-zero = index
+index = 0
 
 for i in range(1, 50000001):
     if i % 10000 == 0:
         print(i)
     for j in range(0, step):
-        index = index.next
-    n = Node(i)
-    index = n.insertafter(index)
-
-index = zero.next
-print(index.num)
+        index = cb[index]
+    tmp = cb[index]
+    cb[index] = i
+    cb[i] = tmp
+    index = cb[index]
+zero = cb[0]
+print(cb[zero])
