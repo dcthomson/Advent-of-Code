30763c995245bbf2ae1123bb926cdfe3ecf1a905
dcthomson <drew@thomson.ninja>
2019-01-14 20:42:01 -0800

    tried array, but wont work without insert

--- 2017/17-Spinlock/17part2.py
+++ 2017/17-Spinlock/17part2.py
@@ -4,20 +4,21 @@ step = int(sys.argv[1])
 
 # use linked list
 
-cb = dict()
+cb = list()
 
-cb[0] = 0
+cb.append(0)
 
 index = 0
 
-for i in range(1, 50000001):
+for i in range(1, 11):
     if i % 10000 == 0:
         print(i)
+    print(index, cb)
     for j in range(0, step):
         index = cb[index]
+    print(index)
     tmp = cb[index]
-    cb[index] = i
+    cb.insert(index, i)
     cb[i] = tmp
-    index = cb[index]
-zero = cb[0]
-print(cb[zero])
+#    index = cb[index]
+print(cb[index])
