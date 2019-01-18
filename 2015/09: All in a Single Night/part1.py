e8f834fabcf6ff73e8e6a6152886981d27d82e24
dcthomson <drew@thomson.ninja>
2019-01-17 16:05:38 -0800

    9.1 complete

--- 2015/09: All in a Single Night/part1.py
+++ 2015/09: All in a Single Night/part1.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import permutations
 
 distances = dict()
 
@@ -11,14 +12,21 @@ with open(sys.argv[1], 'r') as f:
         (p2, dist) = p2.split(" = ")
         if p1 not in distances:
             distances[p1] = dict()
-        distances[p1][p2] = dist
+        distances[p1][p2] = int(dist)
         if p2 not in distances:
             distances[p2] = dict()
-        distances[p2][p1] = dist
+        distances[p2][p1] = int(dist)
 
-def gettotaldist(distances, travelled, total)
+placenum = len(distances)
 
-for k in distances:
-    print(k)
-    print("  ", distances[k])
-    print()
+for path in permutations(list(distances), len(distances)):
+    i = 0
+    dist = 0
+    while i < placenum - 1:
+        dist += distances[path[i]][path[i + 1]]
+
+        i += 1
+    if not shortestdist or dist < shortestdist:
+        shortestdist = dist
+
+print(shortestdist)
