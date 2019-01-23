c4b4e9f12f0b77ea50bce127e988f5aaea4ff09f
dcthomson <drew@thomson.ninja>
2019-01-23 09:02:44 -0800

    17.1 complete

--- 2015/17: No Such Thing as Too Much/part1.py
+++ 2015/17: No Such Thing as Too Much/part1.py
@@ -1,11 +1,31 @@
 import sys
-import itertools
 
-nums = list()
+containers = list()
 
 with open(sys.argv[1], 'r') as f:
     for line in f:
-        nums.append(int(line.strip()))
+        containers.append(int(line.strip()))
 
-for p in itertools.permutations(nums):
-    print(p)
+#containers = [20, 15, 10, 5, 5]
+
+goal = 150
+
+count = 0
+
+import copy
+def combinations(target,data, goal, count):
+    if sum(target) > goal:
+        return count
+    for i in range(len(data)):
+        new_target = copy.copy(target)
+        new_data = copy.copy(data)
+        new_target.append(data[i])
+        new_data = data[i+1:]
+        if sum(new_target) == goal:
+            count += 1
+#            print(new_target)
+        count = combinations(new_target, new_data, goal, count)
+    return count
+
+target = []
+print(combinations(target, containers, goal, count))
