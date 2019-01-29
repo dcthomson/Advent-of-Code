b06bec6d27967649b3ccf000eaa6580ff550ec3d
dcthomson <drew@thomson.ninja>
2019-01-28 17:35:55 -0800

    chenge to list to sort and run big changes first

--- 2015/19: Medicine for Rudolph/part2.py
+++ 2015/19: Medicine for Rudolph/part2.py
@@ -2,18 +2,20 @@ import sys
 
 molecule = ""
 molecules = dict()
-changes = dict()
+changes = list()
 
 with open(sys.argv[1], 'r') as f:
     for line in f:
         line = line.strip()
         if " => " in line:
             (start, end) = line.split(" => ")
-            if start not in changes:
-                changes[start] = list()
-            changes[start].append(end)
+
+            changes.append((end, start))
         elif line != "":
             molecule = line
+
+# lets change big substrings first
+changes.sort(key=lambda tup: len(tup[0]), reverse=True)
 
 molecules = dict()
 
@@ -22,23 +24,22 @@ def changemolecule(mol, steps):
     if mol in molecules and steps >= molecules[mol]:
         return
     molecules[mol] = steps
-    for start in changes:
+    for (end, start) in changes:
         i = 0
 #        print(start)
         while i <= len(mol):
-            for replacement in changes[start]:
-                l = len(replacement)
-                if replacement == mol[i:i + l]:
-                    front = mol[:i]
-                    back = mol[i + l:]
-                    # print(mol)
-                    # print(front, start, back)
-                    # print()
-                    newmol = front + start + back
-                    if newmol == 'e':
-                        print("steps", steps + 1)
-                    else:
-                        changemolecule(newmol, steps + 1)
+            l = len(end)
+            if end == mol[i:i + l]:
+                front = mol[:i]
+                back = mol[i + l:]
+#                print(mol, "--", end, "->", start, "--", front, start, back)
+#                print()
+                newmol = front + start + back
+                if newmol == 'e':
+                    print("steps", steps + 1)
+                else:
+                    changemolecule(newmol, steps + 1)
             i += 1
+#        print("NOTHING ELSE TO CHANGE")
 
 changemolecule(molecule, 0)
