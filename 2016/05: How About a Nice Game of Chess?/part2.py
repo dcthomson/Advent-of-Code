e8302456caf19062dee5caf42a95b4bc3333486d
dcthomson <drew@thomson.ninja>
2019-01-30 18:44:53 -0800

    5.2 complete

--- 2016/05: How About a Nice Game of Chess?/part2.py
+++ 2016/05: How About a Nice Game of Chess?/part2.py
@@ -12,7 +12,15 @@ while len(passwd) < 8:
     result = hashlib.md5(doorid.encode())
     hex = result.hexdigest()
     if hex.startswith("00000"):
-        if hex[5] not in passwd:
-            passwd[hex[5]] = hex[6]
+        try:
+            num = int(hex[5])
+            if 0 <= num <= 7 and hex[5] not in passwd:
+                passwd[hex[5]] = hex[6]
+        except ValueError:
+            pass
     i += 1
-print(passwd)
+passstr = ""
+for i in range(0, 8):
+    passstr += passwd[str(i)]
+
+print(passstr)
