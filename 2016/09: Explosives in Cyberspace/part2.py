c9ef80c73ad35d2f9701e0dc63f13949876184b2
dcthomson <drew@thomson.ninja>
2019-02-03 23:32:38 -0800

    no recursion, just check for "(" and loop

--- 2016/09: Explosives in Cyberspace/part2.py
+++ 2016/09: Explosives in Cyberspace/part2.py
@@ -1,34 +1,40 @@
 import sys
 
+def getdecompress(str):
+    i = 0
+    output = ""
+    marker = False
+#    print(str)
+    while i < len(str):
+        if not marker:
+            # print("  not marker")
+            # print("    i:", i)
+            # print("    output", output)
+            if str[i] == "(":
+                marker = True
+                markerstr = ""
+            else:
+                output += str[i]
+        else:
+            # print("  marker")
+            # print("    i:", i)
+            # print("    output:", output)
+            # print("    markerstr:", markerstr)
+            if str[i] == ")":
+                (chars, times) = markerstr.split("x")
+                for j in range(int(times)):
+                    output += str[i + 1:i + int(chars) + 1]
+                output += str[i + int(chars) + 1:]
+                return output
+            else:
+                markerstr += str[i]
+        i += 1
+
 output = ""
 
 with open(sys.argv[1], 'r') as f:
     for line in f:
-        line = line.strip()
-        i = len(line) - 1
-        marker = False
-        while i >= 0:
-            print(line[i])
-            if not marker:
-                if line[i] == ")":
-                    marker = True
-                    markerstr = ""
-                    markerindex = i + 1
-                else:
-                    output += line[i]
-            else:
-                if line[i] == "(":
-                    marker = False
-                    (chars, times) = markerstr.split("x")
-                    for j in range(int(times) - 1):
-                        print("chars:", chars)
-                        print("output:", output)
-                        print("outpustsplit:", output[-int(chars):])
-                        output += output[-int(chars):]
-                    print("finaloutput:", output)
-                else:
-                    markerstr += line[i]
-            i -= 1
-        print()
-        print(len(output), output[::-1])
-        output = ""
+        output = line.strip()
+        while "(" in output:
+            output = getdecompress(output)
+        print("DONE:", len(output), output)
