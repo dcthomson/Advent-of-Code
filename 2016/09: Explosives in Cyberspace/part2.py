b40eb9dd0c20f98ead8f08341f9a1efe477b85bb
dcthomson <drew@thomson.ninja>
2019-02-07 21:33:16 -0800

    try changing to no function call

--- 2016/09: Explosives in Cyberspace/part2.py
+++ 2016/09: Explosives in Cyberspace/part2.py
@@ -1,5 +1,30 @@
 import sys
 
+output = ""
+
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        str = line.strip()
+        while "(" in str:
+            leftp = str.find("(")
+            rightp = str.find(")")
+            output += str[:leftp]
+            markerstr = str[leftp + 1:rightp]
+            chars, times = markerstr.split("x")
+            chars = int(chars)
+            strtoappend = ""
+            for j in range(int(times)):
+                strtoappend += str[rightp + 1:rightp + chars + 1]
+            output += strtoappend
+            print(str, rightp, chars)
+            str = str[rightp + 1:rightp + int(chars) + 1]
+            print("o:", output)
+            print("s:", str)
+        print("DONE:", output, "\n")
+        output = ""
+
+sys.exit()
+
 def getdecompress(str):
     leftp = str.find("(")
     rightp = str.find(")")
