ecf03a8222cdfc1f3550fd040cda50c8a35a49c0
Thomson <dcthomso@ndc.nasa.gov>
2019-12-13 15:31:46 -0800

    works for first example

--- 2019/12 - The N-Body Problen/part2.py
+++ 2019/12 - The N-Body Problen/part2.py
@@ -1,44 +1,48 @@
 import sys
+from functools import reduce
 
 class Planet:
-    def __init__(self, line):
+    def __init__(self, line, num):
+        self.num = num
         line = line.rstrip().rstrip(">").lstrip("<")
         (x, y, z) = line.split(", ")
-        self.px = int(x.split("=")[1])
-        self.py = int(y.split("=")[1])
-        self.pz = int(z.split("=")[1])
+        self.position = {}
+        self.position["x"] = int(x.split("=")[1])
+        self.position["y"] = int(y.split("=")[1])
+        self.position["z"] = int(z.split("=")[1])
+        self.velocity = {}
+        
+        self.loopstep = {}
+        self.start = {}
+        for c in ("x", "y", "z"):
+            self.velocity[c] = 0
 
-        self.vx = 0
-        self.vy = 0
-        self.vz = 0
+            self.start[c] = (self.position[c], self.velocity[c])
+
+            self.loopstep[c] = False
+
+        print(self)
 
     def setvelocity(self, p):
-        if p.px > self.px:
-            self.vx += 1
-        elif p.px < self.px:
-            self.vx -= 1
-        if p.py > self.py:
-            self.vy += 1
-        elif p.py < self.py:
-            self.vy -= 1
-        if p.pz > self.pz:
-            self.vz += 1
-        elif p.pz < self.pz:
-            self.vz -= 1
+        for c in ("x", "y", "z"):
+            if p.position[c] > self.position[c]:
+                self.velocity[c] += 1
+            elif p.position[c] < self.position[c]:
+                self.velocity[c] -= 1
+
     
     def moveplanet(self):
-        self.px += self.vx
-        self.py += self.vy
-        self.pz += self.vz
+        for c in ("x", "y", "z"):
+            self.position[c] += self.velocity[c]
 
     def __str__(self):
-        ret = "pos=<x=" + str(self.px)
-        ret += ", y=" + str(self.py)
-        ret += ", z=" + str(self.pz)
+        ret = str(self.num) + ": pos=<x=" + str(self.position['x'])
+        ret += ", y=" + str(self.position['y'])
+        ret += ", z=" + str(self.position['z'])
         ret += ">, "
-        ret += "vel=<x=" + str(self.vx)
-        ret += ", y=" + str(self.vy)
-        ret += ", z=" + str(self.vz)
+        ret += "vel=<x=" + str(self.velocity['x'])
+        ret += ", y=" + str(self.velocity['y'])
+        ret += ", z=" + str(self.velocity['z'])
         ret += ">"
         return ret
 
@@ -46,42 +50,77 @@ class Planet:
         return self.getpotentialenergy() * self.getkineticenergy()
 
     def getpotentialenergy(self):
-        return abs(self.px) + abs(self.py) + abs(self.pz)
+        pe = 0
+        for c in ("x", "y", "z"):
+            pe += abs(self.position[c])
+        return pe
 
     def getkineticenergy(self):
-        return abs(self.vx) + abs(self.vy) + abs(self.vz)
+        ke = 0
+        for c in ("x", "y", "z"):
+            ke += abs(self.velocity[c])
+        return ke
 
     def shortstring(self):
-        ss = "x" + str(self.px)
-        ss += "y" + str(self.py)
-        ss += "z" + str(self.pz)
-        ss += "x" + str(self.vx)
-        ss += "y" + str(self.vy)
-        ss += "z" + str(self.vz)
+        ss = "x" + str(self.position['x'])
+        ss += "y" + str(self.position['y'])
+        ss += "z" + str(self.position['z'])
+        ss += "x" + str(self.velocity['x'])
+        ss += "y" + str(self.velocity['y'])
+        ss += "z" + str(self.velocity['z'])
         return ss
+
+    def checkforloop(self, step):
+        for c in ("x", "y", "z"):
+            if step > 0:
+                if not self.loopstep[c]:
+                    if self.start[c] == (self.position[c], self.velocity[c]):
+                        self.loopstep[c] = step
+                        
+                        print("STEP", step)
+                        print(self)
+
+    def allLooped(self):
+        ret = True
+        for c in ("x", "y", "z"):
+            if not self.loopstep[c]:
+                ret = False
+        return ret
+
+def lowestcommonmultiple(l):
+    l.sort()
+    largest = l.pop()
+    current = largest
+    found = False
+    while found == False:
+        # print(current)
+        found = True
+        for i in l:
+            if current % i != 0:
+                found = False
+                break
+        if not found:
+            current += largest
+    return current
+
 
 planets = []
 
-universes = {}
-
 with open(sys.argv[1]) as f:
+    num = 1
     for line in f:
-        planets.append(Planet(line))
-
+        planets.append(Planet(line, num))
+        num += 1
+print()
 count = 0
-while True:
-    if not count % 100000:
-        print(count)
-    curruniverse = ""
-    for p in planets:
-        curruniverse += p.shortstring()
+allLooped = False
+while allLooped == False:
 
-    if curruniverse in universes:
-        print(count)
-        print(curruniverse)
-        break
-    else:
-        universes[curruniverse] = True
+    allLooped = True
+    for p in planets:
+        p.checkforloop(count)
+        if not p.allLooped():
+            allLooped = False
 
     for i in planets:
         for j in planets:
@@ -92,8 +131,15 @@ while True:
         p.moveplanet()
     
     count += 1
-    
-# totalenergy = 0
-# for p in planets:
-#     totalenergy += p.getenergy()
-# print(totalenergy)
+
+
+steps = {}
+for p in planets:
+    for c in ("x", "y", "z"):
+        steps[p.loopstep[c]] = True
+
+steps = list(steps.keys())
+
+print(steps)
+
+print(lowestcommonmultiple(steps))
