263433b4d44b283a93b9b8504f314bd6ede45fbe
Thomson <dcthomso@ndc.nasa.gov>
2019-12-16 11:14:41 -0800

    16 part1 done

--- 2019/16 - Flawed Frequency Transmission/part1.py
+++ 2019/16 - Flawed Frequency Transmission/part1.py
@@ -5,11 +5,36 @@ basepattern = [0, 1, 0, -1]
 with open(sys.argv[1]) as f:
     signal = f.readline()
     # turn string insto a list of ints
-    l = list(map(int, list(signal)))
+    signal = list(map(int, list(signal)))
+    signalsize = len(signal)
 
-    for i in range(1, len(l) + 1):
-        j = i
-        if i == 1:
-            j += 1
-        for j in range(0, j):
-            l[i - 1] * basepattern[j]
+    basepatsize = len(basepattern)
+
+    newsignal = []
+
+    for num in range(0, 100):
+        # print(num)
+        repeatcount = 0
+        newsignal = []
+        for k in range(0, signalsize):
+            j = 0
+            patternindex = 0
+            total = 0
+            for i in range(0, signalsize):
+
+                if j == repeatcount:
+                    j = 0
+                    patternindex += 1
+                    if patternindex >= basepatsize:
+                        patternindex = 0
+                else:
+                    j += 1
+                total += signal[i] * basepattern[patternindex]
+                # print(signal[i],"*", basepattern[patternindex], end="   +   ")
+            repeatcount += 1
+            # print(int(str(total)[-1]))
+            newsignal.append(int(str(total)[-1]))
+            # print(newsignal)
+        signal = newsignal
+
+    print("".join(str(x) for x in signal)[0:8])
