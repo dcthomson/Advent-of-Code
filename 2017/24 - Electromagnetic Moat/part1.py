77565309f303e2c9e8fa1522e253cc582fd69f78
Thomson <dcthomso@ndc.nasa.gov>
2019-12-23 15:56:31 -0800

    example working with component class

--- 2017/24 - Electromagnetic Moat/part1.py
+++ 2017/24 - Electromagnetic Moat/part1.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 
 class Component:
 
@@ -6,31 +7,25 @@ class Component:
         self.port1 = l
         self.port2 = r
 
+    def __str__(self):
+        return str(self.port1) + "/" + str(self.port2)
+
 class Bridge:
 
     def __init__(self):
         self.components = list()
-        self.open = False
+        self.open = 0
 
     def addcomponent(self, component):
-        if len(self.components) == 0:
-            if component.part1 == 0:
-                self.components.append(component)
-                self.open = component.part2
-            elif component.part2 == 0:
-                self.components.append(component)
-                self.open = component.part1
-            else:
-                return False
+
+        if component.port1 == self.open:
+            self.components.append(component)
+            self.open = component.port2
+        elif component.port2 == self.open:
+            self.components.append(component)
+            self.open = component.port1
         else:
-            if component.port1 == self.open:
-                self.components.append(component)
-                self.open = component.port2
-            elif component.port2 == self.open:
-                self.components.append(component)
-                self.open = component.port1
-            else:
-                return False
+            return False
         return True
 
     def getstrength(self):
@@ -38,6 +33,14 @@ class Bridge:
         for i in self.components:
             strength += i.port1 + i.port2
         return strength
+
+    def __str__(self):
+        retstr = ""
+        for c in self.components:
+            if retstr != "":
+                retstr += "--"
+            retstr += str(c.port1) + "/" + str(c.port2)
+        return retstr
 
 components = list()
 
@@ -47,13 +50,23 @@ with open(sys.argv[1], 'r') as f:
         l = line.split("/")
         components.append(Component(int(l[0]), int(l[1])))
 
-print(components)
+strongestbridge = 0
 
-def addcomponent(components, bridge=Bridge()):
+def buildbridge(components, bridge=Bridge()):
+    global strongestbridge
+    # cs = components.copy()
+    # print(bridge)
+    bs = bridge.getstrength()
+    if bs > strongestbridge:
+        strongestbridge = bs
+        print(bs)
+    # print(strongestbridge)
+    for component in components:
+        b = copy.deepcopy(bridge)
+        if b.addcomponent(component):
+            buildbridge([c for c in components if c != component], b)
 
-for component in components:
-    b = Bridge()
-    if b.addcomponent(component):
-        addcomponent(components)
 
-print(strength)
+
+buildbridge(components)
+print(strongestbridge)
