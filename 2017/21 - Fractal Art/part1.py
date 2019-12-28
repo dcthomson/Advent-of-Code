c934328c7b67c506e04aed9b11c6660abd941d59
Thomson <dcthomso@ndc.nasa.gov>
2019-12-27 16:12:19 -0800

    21 start

--- 2017/21 - Fractal Art/part1.py
+++ 2017/21 - Fractal Art/part1.py
@@ -1,12 +1,112 @@
 import sys
 
+# startimage = "#./#."
+# startimage = ".#./..#/###"
+# startimage = "##../##../..##/..##"
+startimage = "###.../###.../###.../...###/...#.#/...###"
+
 class Image:
 
-    def __init__(self, s=False):
-        if s:
-            i = 0
-            for i in s.split("/"):
-                pass
+    def __init__(self, s):
+        self.s = s
+
+    def printimage(self):
+        for row in self.s.split("/"):
+            print(row)
+
+    def divide(self):
+        rows = self.s.split("/")
+        size = 0
+        if not len(rows[0]) % 2:
+            size = 2
+        else:
+            size = 3
+        squares = []
+        rownum = 0
+        tmplines = []
+        for row in rows:
+            tmplines.append([row[i:i+size] for i in range(0, len(row), size)])
+            rownum += 1
+            if not rownum % size:
+                tmpsquares = []
+                for i in range(0, len(tmplines[0])):
+                    sqr = ""
+                    for l in tmplines:
+                        sqr += l[i]
+                        sqr += "/"
+                    sqr = sqr.rstrip("/")
+                    tmpsquares.append(sqr)
+                tmplines = []
+                squares.append(tmpsquares)
+
+        return squares
+    
+    # def combine(self, squares):
+
+
+    def getnumon(self):
+        return self.s.count("#")
+
+    def flipandrotate(self):
+        # returns list of all possible (8) rotated and flipped variations of image
+        
+        # check if 2x2 or 3x3
+        if len(self.s) != 11 and len(self.s) != 5:
+            return False
+
+        perms = []
+
+        def _flip(s):
+            rows = []
+            for r in s.split("/"):
+                rows.append(r)
+            newrows = rows.copy()
+            newrows[0] = rows[-1]
+            newrows[-1] = rows[0]
+            return "/".join(newrows)
+
+        def _rotate(s):
+            rows = []
+            for r in s.split("/"):
+                rows.append(list(r))
+            newrows = []
+            top = rows[0]
+            bottom = rows[-1]
+            left = []
+            right = []
+            for r in rows:
+                left.append(r[0])
+                right.append(r[-1])
+            left.reverse()
+            right.reverse()
+            newrows.append(left)
+            if len(rows) == 3:
+                mid = []
+                mid.append(bottom[1])
+                mid.append(rows[1][1])
+                mid.append(top[1])
+                newrows.append(mid)
+            newrows.append(right)
+
+            s = ""
+            for row in newrows:
+                s += "".join(row)
+                s += "/"
+            s = s.rstrip("/")
+
+            return s
+        
+        perms.append(self.s)
+        perms.append(_flip(self.s))
+        perms.append(_rotate(self.s))
+        perms.append(_flip(_rotate(self.s)))
+        perms.append(_rotate(_rotate(self.s)))
+        perms.append(_flip(_rotate(_rotate(self.s))))
+        perms.append(_rotate(_rotate(_rotate(self.s))))
+        perms.append(_flip(_rotate(_rotate(_rotate(self.s)))))
+        return perms
+        
+                       
 
 rules = dict()
 
@@ -16,3 +116,32 @@ with open(sys.argv[1], 'r') as f:
         rule, out = line.split(" => ")
         rules[rule] = out
 
+image = Image(startimage)
+
+image.printimage()
+print("\n")
+perms = image.flipandrotate()
+
+if perms:
+    for perm in perms:
+        i = Image(perm)
+        i.printimage()
+        print()
+
+print(image.divide())
+newsquares = []
+squares = image.divide()
+for rownum in range(0, len(squares)):
+    newsquares.append([])
+    for i in range(0, len(squares[rownum])):
+        print(squares[rownum][i])
+        img = Image(squares[rownum][i])
+        for perm in img.flipandrotate():
+            if perm in rules:
+                print(perm, "=> ", end="")
+                img.s = rules[perm]
+                print(img.s)
+                newsquares[0].append(img.s)
+                break
+
+print(newsquares)
