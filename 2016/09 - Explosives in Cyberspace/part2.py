5966e3d44c3248a9b9bf6cb3b64a10af4ad56cd4
Thomson <dcthomso@ndc.nasa.gov>
2020-01-07 14:01:07 -0800

    part2 done

--- 2016/09 - Explosives in Cyberspace/part2.py
+++ 2016/09 - Explosives in Cyberspace/part2.py
@@ -1,21 +1,30 @@
 import sys
 
-output = ""
+strlist = []
 
 with open(sys.argv[1], 'r') as f:
     for line in f:
-        str = line.strip()
-        while "(" in str:
-            print(len(str),str.count('('))
-            output = ""
-            leftp = str.find("(")
-            rightp = str.find(")")
-            output += str[:leftp]
-            markerstr = str[leftp + 1:rightp]
-            chars, times = markerstr.split("x")
-            chars = int(chars)
-            strtoappend = str[rightp + 1:rightp + chars + 1]
-            output += strtoappend * int(times)
-            output += str[rightp + chars + 1:]
-            str = output
-        print(len(output))
+        s = line.strip()
+
+for c in s:
+    strlist.append([c, 1])
+
+weight = 0
+
+i = 0
+while i < len(strlist):
+    if strlist[i][0] == "(":
+        marker = ""
+        i += 1
+        while strlist[i][0] != ")":
+            marker += strlist[i][0]
+            i += 1
+        length, multiplier = list(map(int, marker.split("x")))
+        for j in range(i, i + length):
+            j += 1
+            strlist[j][1] *= multiplier
+    if strlist[i][0] != ")":
+        weight += strlist[i][1]
+    i += 1
+
+print(weight)
