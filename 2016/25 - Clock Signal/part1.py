f3e8c012b9e47e1cd2b6a9f8c4a50413c2ef83c4
Thomson <dcthomso@ndc.nasa.gov>
2020-01-10 12:49:06 -0800

    done

---
+++ 2016/25 - Clock Signal/part1.py
@@ -0,0 +1,77 @@
+import sys
+
+instructions = []
+
+with open(sys.argv[1]) as f:
+    for line in f:
+        instructions.append(line.strip())
+
+regs = {'a': 1}
+# regs = {}
+num = 1
+while True:
+    print(num)
+    regs = {'a': num}
+    out = []
+    instindex = 0
+    while instindex < len(instructions):
+        # print(instructions[instindex])
+        try:
+            (command, val1, val2) = instructions[instindex].split()
+        except:
+            (command, val1) = instructions[instindex].split()
+        try:
+            if command == 'cpy':
+                try:
+                    regs[val2] = int(val1)
+                except:
+                    regs[val2] = regs[val1]
+            elif command == 'inc':
+                regs[val1] += 1
+            elif command == 'dec':
+                regs[val1] -= 1
+            elif command == 'jnz':
+                jump = False
+                try:
+                    if int(val1) != 0:
+                        jump = True
+                except:
+                    if val1 not in regs:
+                        jump = False
+                    elif regs[val1] != 0:
+                        jump = True
+                if jump:
+                    try:
+                        instindex += int(val2)
+                    except:
+                        instindex += regs[val2]
+                    continue
+            elif command == 'tgl':
+                changeindex = instindex + regs[val1]
+                instarr = instructions[changeindex].split()
+
+                if len(instarr) == 2:
+                    if instarr[0] == 'inc':
+                        instructions[changeindex] = 'dec ' + instarr[1]
+                    else:
+                        instructions[changeindex] = 'inc ' + instarr[1]
+                elif len(instarr) == 3:
+                    if instarr[0] =='jnz':
+                        instructions[changeindex] = 'cpy ' + instarr[1] + " " + instarr[2]
+                    else:
+                        instructions[changeindex] = 'jnz ' + instarr[1] + " " + instarr[2]
+            elif command == 'out':
+                if regs[val1] != 1 and regs[val1] != 0:
+                    break
+                if not len(out):
+                    out.append(regs[val1])
+                elif regs[val1] == out[-1]:
+                    break
+                else:
+                    out.append(regs[val1])
+        except:
+            pass
+        instindex += 1
+        # print(instructions)
+        # print(regs)
+    num += 1
