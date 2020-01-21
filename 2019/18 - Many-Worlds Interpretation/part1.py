c70e6a147d20ae45e63a78d5ac734c61503aacc3
Thomson <dcthomso@ndc.nasa.gov>
2020-01-21 10:32:30 -0800

    works, just slow

--- 2019/18 - Many-Worlds Interpretation/part1.py
+++ 2019/18 - Many-Worlds Interpretation/part1.py
@@ -1,5 +1,7 @@
 import sys
 import string
+import collections
+import time
 
 maze = {}
 doors = {}
@@ -21,41 +23,57 @@ with open(sys.argv[1]) as f:
             x += 1
         y += 1
 
-print(keys)
-print(doors)
-print(maze)
-            
+# print(keys)
+# print(doors)
+# print(maze)
 
-visited = {}
-Q = [current]
+def stringify(node):
+    return "%s-%s-%s" % (node[0][0], node[0][1], "".join(node[1]))
+
+longest = 0
+
+t0 = time.time()
+
+visited = []
+Q = collections.OrderedDict()
+Q[stringify(current)] = 1
 while Q:
-    node = Q.pop(0)
+    node = Q.popitem(last=False)
+    steps = node[1]
+    node = node[0]
     if node not in visited:
-        print(node)
-        visited[node] 
-        nodecoord = node[0]
-        newkeys = False
+        # print(steps, node)
+        visited.append(node)
+        nodelist = node.split("-") 
+        nodecoord = (int(nodelist[0]), int(nodelist[1]))
+        nodekeys = list(nodelist[2])
         neighbors = []
         for coord in ((nodecoord[0] + 1, nodecoord[1]),
                       (nodecoord[0] - 1, nodecoord[1]),
                       (nodecoord[0], nodecoord[1] + 1),
                       (nodecoord[0], nodecoord[1] - 1)):
+            newkeys = False
             if coord in maze:
                 if maze[coord] != "#":
-                    if maze[coord] == ".":
-                        pass
                     if maze[coord] in string.ascii_lowercase:
-                        if maze[coord] not in node[1]:
-                            newkeys = node[1].copy()
+                        if maze[coord] not in nodekeys:
+                            newkeys = nodekeys.copy()
                             newkeys.append(maze[coord])
                             newkeys.sort()
+                            # check if done
+                            if len(newkeys) == len(keys):
+                                print("DONE:", steps)
+                                exit()
+                            if len(newkeys) > longest:
+                                print(round(time.time() - t0, 2), steps, newkeys)
+                                longest = len(newkeys)
                     elif maze[coord] in string.ascii_uppercase:
-                        if maze[coord].lower() not in node[1]:
+                        if maze[coord].lower() not in nodekeys:
                             # found door without key :(
                             continue
-                if newkeys:
-                    neighbors.append((coord, newkeys))
-                else:
-                    neighbors.append((coord, node[1]))
+                    if newkeys:
+                        neighbors.append((coord, newkeys))
+                    else:
+                        neighbors.append((coord, nodekeys))
         for neighbor in neighbors:
-            Q.append(neighbor)
+            Q[stringify(neighbor)] = steps + 1
