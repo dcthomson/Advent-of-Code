e6f2e28792ad7ffaac9e07f3122bcc85ba7a7dc4
Drew Thomson <drew@thomson.ninja>
2022-07-27 08:05:23 -0700

    Whales done

---
+++ 2021/07 - The Treachery of Whales/part2.py
@@ -0,0 +1,32 @@
+import sys
+
+low = False
+high = False
+
+horizontalpositions = list()
+
+with open(sys.argv[1], "r") as f:
+    for line in f:
+        for num in line.rstrip().split(','):
+            num = int(num)
+            if not low or num < low:
+                low = num
+            if not high or num > high:
+                high = num
+            horizontalpositions.append(num)
+
+totals = dict()
+
+for i in range(low, high + 1):
+    fuel = 0
+    for hpos in horizontalpositions:
+        fuel += int((abs(hpos - i) * (abs(hpos - i) + 1)) / 2)
+    totals[i] = fuel
+
+lowest = False
+
+for k in totals:
+    if not lowest or totals[k] < lowest:
+        lowest = totals[k]
+
+print(lowest)
