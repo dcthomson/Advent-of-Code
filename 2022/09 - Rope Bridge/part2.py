0a173b2e517e35545ac88fb6adb9b2cfc42c3bed
Drew Thomson <drew@thomson.ninja>
2022-12-20 20:46:58 -0800

    need to fix how secondary and later rope segments move

--- 2022/09 - Rope Bridge/part2.py
+++ 2022/09 - Rope Bridge/part2.py
@@ -1,12 +1,38 @@
 import sys
 
+def printrope(rope):
+
+    minx = maxx = miny = maxy = 0
+
+    for segment in rope:
+        x = segment[0]
+        y = segment[1]
+        if x > maxx:
+            maxx = x
+        elif x < minx:
+            minx = x
+        elif y > maxy:
+            maxy = y
+        elif y < miny:
+            miny = y
+
+        for y in range(miny, maxy+1):
+            for x in range(minx, maxx+1):
+                for i in range(0, 10):
+                    if rope[i] == (x,y):
+                        if i == 0:
+                            print("H", end="")
+                            break
+                        else:
+                            print(i, end="")
+                            break
+            print()
+
 with open(sys.argv[1], "r") as f:
 
-    
-
     rope = []
-    for i in range(0, 9):
-        rope[i] = (0,0)
+    for i in range(0, 10):
+        rope.append((0,0))
 
     dirs = {"R": (1,0),
             "L": (-1,0),
@@ -22,13 +48,20 @@ with open(sys.argv[1], "r") as f:
 
         (dir, steps) = line.split()
 
-        for i in range(0, int(steps)):
-            x = head[0] + dirs[dir][0]
-            y = head[1] + dirs[dir][1]
-            if abs(x - tail[0]) >= 2 or abs(y - tail[1]) >= 2:
-                tail = head
-            head = (x,y)
-            if tail not in visited:
-                visited[tail] = True
+        for _ in range(0, int(steps)):
+            for i in range(0,9):
+                if i == 0:
+                    x = rope[i][0] + dirs[dir][0]
+                    y = rope[i][1] + dirs[dir][1]
+                else:
+                    x = rope[i][0]
+                    y = rope[i][1]
+                print(x,y,rope)
+                if abs(x - rope[i + 1][0]) >= 2 or abs(y - rope[i + 1][1]) >= 2:
+                    rope[i + 1] = rope[i]
+                rope[i] = (x,y)
+            if rope[9] not in visited:
+                visited[rope[9]] = True
+            printrope(rope)
 
 print(len(visited))
