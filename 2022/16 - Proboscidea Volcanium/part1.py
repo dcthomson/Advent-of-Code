832782ed95b5120bc32dc102d156b858012c29f1
Drew Thomson <drew@thomson.ninja>
2022-12-20 20:48:24 -0800

    day 16 started

---
+++ 2022/16 - Proboscidea Volcanium/part1.py
@@ -0,0 +1,35 @@
+import sys
+
+class Valve():
+
+    def __init__(self, name, flow, valves):
+        self.name = name
+        self.flow = flow
+        self.valves = valves
+
+valves = {}
+
+with open(sys.argv[1], "r") as f:
+
+    for line in f:
+        line = line.strip()
+
+        (first, last) = line.split("; tunnels lead to valves ")
+        othervalves = last.split(", ")
+        (_,name,_,_,flow) = first.split()
+        flow = flow.split("=")[1]
+
+        valves[name] = Valve(name, flow, othervalves)
+
+
+def moveandrelease(valve, open = [], pressure = 0, move = 0):
+
+    for v in valves[valve].valves:
+        p = moveandrelease(v, open, pressure, move)
+        
+
+    for v in valves[valve].valves:
+        open.append(valve)
+        moveandrelease(v, open, pressure, move)
+
+print(moveandrelease("AA"))
