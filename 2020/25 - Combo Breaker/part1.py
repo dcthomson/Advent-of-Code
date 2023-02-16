f434f1ce993ec26ad6f214ccb78a66daa6ad6d14
Drew Thomson <drew@thomson.ninja>
2023-02-16 00:21:32 -0800

    part 1 done

--- 2020/25 - Combo Breaker/part1.py
+++ 2020/25 - Combo Breaker/part1.py
@@ -1,54 +1,26 @@
 import sys
 
-tiles = {}
+door = False
+key = False
 
 with open(sys.argv[1], "r") as f:
     for line in f:
         line = line.rstrip()
-        north = False
-        south = False
-        x = y = z = 0
-        for c in line:
-            if c == "n":
-                north = True
-            elif c == "s":
-                south = True
-            elif c == "e":
-                if north:
-                    x += 1
-                    z -= 1
-                    north = False
-                elif south:
-                    y -= 1
-                    z += 1
-                    south = False
-                else:
-                    x += 1
-                    y -= 1
-            elif c == "w":
-                if north:
-                    y += 1
-                    z -= 1
-                    north = False
-                elif south:
-                    x -= 1
-                    z += 1
-                    south = False
-                else:
-                    x -= 1
-                    y += 1
-        
-        if (x, y, z) in tiles:
-            if tiles[(x, y, z)] == "W":
-                tiles[(x, y, z)] = "B"
-            else:
-                tiles[(x, y, z)] = "W"
+        if not key:
+            key = int(line)
         else:
-            tiles[(x, y, z)] = "B"
+            door = int(line)
 
-blacktiles = 0
-for k in tiles:
-    if tiles[k] == "B":
-        blacktiles += 1
+subnum = 7
+i = 1
+num = 0
+while i != key:
+    i = (i*subnum) % 20201227
+    num += 1
+print(num)
 
-print(blacktiles)
+subnum = door
+i = 1
+for _ in range(0, num):
+    i = (i*subnum) % 20201227
+print(i)
