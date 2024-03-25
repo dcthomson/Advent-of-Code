7c173af8a5401b44187636a36744ef9c83d5c6be
Drew Thomson <drew@thomson.ninja>
2024-03-25 00:07:27 -0700

    start

---
+++ 2023/15 - Lens Library/part2.py
@@ -0,0 +1,30 @@
+import sys
+
+steps = []
+
+class Box:
+
+    def __init__(self):
+        self.lenses = {}
+
+with open(sys.argv[1], "r") as f:
+
+    for line in f:
+        line = line.strip()
+
+        steps = line.split(",")
+
+
+
+total = 0
+
+
+
+for step in steps:
+    val = 0
+    for c in step:
+        val += ord(c)
+        val *= 17
+        val = val % 256
+    total += val
+print(total)
