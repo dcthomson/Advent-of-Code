6704da14b1e4f94cf467a08ab96a011d04267edc
dcthomson <drew@thomson.ninja>
2024-11-01 11:13:31 -0700

    add subpacket

--- 2021/16 - Packet Decoder/part1.py
+++ 2021/16 - Packet Decoder/part1.py
@@ -7,7 +7,8 @@ binstr = ""
 with open(sys.argv[1], "r") as f:
     for line in f:
         hexstr = line.rstrip()
-        binstr = '{0:b}'.format(int(hexstr, 16)) 
+        h_size = len(hexstr) * 4
+        binstr = ( bin(int(hexstr, 16))[2:] ).zfill(h_size)
 
 print(binstr)
 
@@ -18,6 +19,7 @@ def parsepacket(binstr):
     global versiontotal
 
     pversion = binstr[0:3]
+    typeid = binstr[3:6]
     binstr = binstr[3:]
     versiontotal += int(pversion, 2)
 
@@ -33,11 +35,27 @@ def parsepacket(binstr):
             binstr = binstr[5:]
         literal += binstr[1:5]
         binstr = binstr[5:]
+        print("version:", int(pversion, 2))
+        print("type ID:", int(typeid, 2))
         print("literal:", int(literal, 2))
+        return binstr
     
     else:
-        # not literal
-        i = 0
+        # not literal (has sub packets)
+        subpackettype = int(binstr[0])
+        binstr = binstr[1:]
+        if not subpackettype:
+            # 15 bit subpacket
+            subpackettotal = binstr[0:15]
+            parsepacket(binstr[15:])
+        
+        else:
+            # 11 bit subpacket
+            subpacketcount = binstr[0:11]
+            binstr = binstr[11:]
+            for i in range(0, 3):
+                binstr = parsepacket(binstr)
+
 
 parsepacket(binstr)
 
