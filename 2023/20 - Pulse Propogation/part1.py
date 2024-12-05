ebf8a2646d155d2587936f062f29e07577fc9c72
dcthomson <drew@thomson.ninja>
2024-12-05 15:15:52 -0800

    add pulse count

--- 2023/20 - Pulse Propogation/part1.py
+++ 2023/20 - Pulse Propogation/part1.py
@@ -2,17 +2,29 @@ import sys
 
 modules = {}
 
+highcount = 0
+lowcount = 0
+
 class Module:
     def __init__(self, name, next):
         self.name = name
         self.nextmodules = next
+
+    def countpulse(self, pulse):
+        global highcount
+        global lowcount
+        if pulse == "low":
+            lowcount += 1
+        elif pulse == "high":
+            highcount += 1
 
 class Flipflop(Module):
     def __init__(self, name, next):
         super().__init__(name, next)
         self.state = "off"
 
-    def run(self, pulse, q, input):       
+    def run(self, pulse, q, input):
+        super().countpulse(pulse)
         if pulse == "low":
             if self.state == "off":
                 self.state = "on"
@@ -31,6 +43,7 @@ class Conjunction(Module):
         self.inputs = {}
 
     def run(self, pulse, q, input):
+        super().countpulse(pulse)
         self.inputs[input] = pulse
         allhigh = True
         for i in self.inputs:
@@ -51,25 +64,32 @@ class Broadcast(Module):
         super().__init__(name, next)
 
     def run(self, pulse, q, input):
+        super().countpulse(pulse)
         for nextmodule in self.nextmodules:
             q.append((nextmodule, pulse, self.name))
         return q
 
+nextlist = []
 
 with open(sys.argv[1], "r") as f:
     for line in f:
         line = line.strip()
 
-        (m, next) = line.split(" -> ")
-        nextlist = next.split(", ")
+        (m, n) = line.split(" -> ")
+        next = n.split(", ")
+        nextlist.extend(next)
         if m.startswith("&"):
             modulename = m[1:]
-            modules[modulename] = Conjunction(modulename, nextlist)
+            modules[modulename] = Conjunction(modulename, next)
         elif m.startswith("%"):
             modulename = m[1:]
-            modules[modulename] = Flipflop(modulename, nextlist)
+            modules[modulename] = Flipflop(modulename, next)
         else:
-            modules[m] = Broadcast(m, nextlist)
+            modules[m] = Broadcast(m, next)
+
+for n in nextlist:
+    if n not in modules:
+        modules[n] = Flipflop(n, [])
 
 
 for i in range(0, 1000): 
@@ -77,14 +97,15 @@ for i in range(0, 1000):
     queue = [("broadcaster", "low", "")]
 
     while(queue):
-        print(queue)
+        # print(queue)
         (module, pulse, input) = queue.pop(0)
-        if module != "output":
-            queue = modules[module].run(pulse, queue, input)
+        queue = modules[module].run(pulse, queue, input)
 
-    for m in modules:
-        try:
-            print(modules[m].name, ": ", modules[m].state)
-        except:
-            pass
-    print()
+    # for m in modules:
+    #     try:
+    #         print(modules[m].name, ": ", modules[m].state)
+    #     except:
+    #         pass
+    # print()
+
+print(lowcount, "low *", highcount, "high =", lowcount * highcount)
