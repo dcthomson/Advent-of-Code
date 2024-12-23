3703c83d93621214f6fbf2cf2d19b8adde8fa735
dcthomson <drew@thomson.ninja>
2024-12-23 11:57:26 -0800

    part 2 done

--- 2024/22 - Monkey Market/part2.py
+++ 2024/22 - Monkey Market/part2.py
@@ -1,22 +1,40 @@
 import sys
 
-nums = []
+class seller:
+    def __init__(self, num):
+        self.num = num
+        self.changes = []
+        self.sequences = {}
+
+    def updatesequences(self):
+        for n, change in enumerate(self.changes):
+            try:
+                changeset = (self.changes[n+1]-self.changes[n],
+                             self.changes[n+2]-self.changes[n+1],
+                             self.changes[n+3]-self.changes[n+2],
+                             self.changes[n+4]-self.changes[n+3])
+                if changeset not in self.sequences:
+                    self.sequences[changeset] = self.changes[n+4]
+            except:
+                pass
+
+sellers = []
 
 with open(sys.argv[1], "r") as f:
 
     for line in f:
         line = line.strip()
 
-        nums.append(int(line))
+        sellers.append(seller(int(line)))
 
 total = 0
 
-nums = [123]
 
-for num in nums:
+for seller in sellers:
     # num = int(str(num)[-1])
-    for _ in range(0, 10):
-        print(int(str(num)[-1]))
+    num = seller.num
+    for _ in range(0, 2000):
+        seller.changes.append(int(str(num)[-1]))
         i = num * 64
         num = i ^ num
         num = num % 16777216
@@ -30,3 +48,27 @@ for num in nums:
         num = num % 16777216
 
     # print(int(str(num)[-1]))
+    seller.updatesequences()
+    # print(seller.changes)
+    # print(seller.sequences)
+
+sequences = {}
+
+for seller in sellers:
+    for sequence in seller.sequences:
+        sequences[sequence] = 0
+
+for sequence in sequences:
+    for seller in sellers:
+        try:
+            sequences[sequence] += seller.sequences[sequence]
+        except:
+            pass
+
+bananas = 0
+
+for sequence in sequences:
+    if sequences[sequence] > bananas:
+        bananas = sequences[sequence]
+
+print(bananas)
