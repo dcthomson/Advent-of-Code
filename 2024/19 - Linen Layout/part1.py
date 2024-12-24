ded24895bbb9becebd8962ec2e81a61dde4db395
dcthomson <drew@thomson.ninja>
2024-12-24 10:54:00 -0800

    reverted and fixed part 1

--- 2024/19 - Linen Layout/part1.py
+++ 2024/19 - Linen Layout/part1.py
@@ -4,28 +4,13 @@ towels = []
 patterns = []
 
 with open(sys.argv[1], "r") as f:
-
-    blocks = 0
     for line in f:
         line = line.strip()
-
         if "," in line:
             towels = line.split(", ")
         elif line != "":
             patterns.append(line)
 
-def beginswith(s, count=0, newtowels=towels.copy()):
-
-    if s == "":
-        count += 1
-    else:
-        for towel in towels:
-            if towel in s:
-                newtowels.append(towel)
-        for towel in newtowels:
-            if s.startswith(towel):
-                count = beginswith(s[len(towel):], count, newtowels.copy())
-    return count
 def beginswith(s):
     # print(s)
     if s == "":
@@ -41,10 +26,6 @@ def beginswith(s):
 count = 0
 
 for pattern in patterns:
-    print(pattern, "", end="", flush=True)
-    pcount = beginswith(pattern)
-    print(pcount)
-    count += pcount
     if beginswith(pattern):
         count += 1
 
