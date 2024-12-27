8da1159b614fd5cc37529e7402af2c4863672048
Drew Thomson <drew@thomson.ninja>
2024-12-26 19:39:17 -0800

    fixed manhatten and consolodated start and end distances

--- 2024/20 - Race Condition/part2.py
+++ 2024/20 - Race Condition/part2.py
@@ -1,7 +1,7 @@
 import sys
 from collections import defaultdict
 
-x = y = xmax = ymax = 0
+y = 0
 start = end = None
 grid = {}
 
@@ -18,78 +18,65 @@ with open(sys.argv[1], "r") as f:
             elif c == "E":
                 end = (x,y)
             x += 1
-        xmax = x
         y += 1
-    ymax = y
 
 # loop through every available space use manhatten
 # distance to check cheat distance then check distance
 # from start to coord and end to cheat coord
 
-q = []
-q.append(start)
-ssteps = {}
-steps = 0
-ssteps[start] = steps
-dirs = {"N":(0,-1), 
-        "E":(1,0),
-        "S":(0,1),
-        "W":(-1,0)}
+def getsteps(startcoord, grid):
+    q = []
+    q.append(startcoord)
+    stepsdict = {}
+    steps = 0
+    stepsdict[startcoord] = steps
+    dirs = [(0,-1),(1,0),(0,1),(-1,0)]
 
-while q:
-    current = q.pop(0)
-    steps += 1
-    for d in dirs.values():
-        next = (current[0] + d[0], current[1] + d[1])
-        if next not in ssteps and grid[next] != "#":
-            q.append(next)
-            ssteps[next] = steps
+    while q:
+        current = q.pop(0)
+        steps += 1
+        for d in dirs:
+            next = (current[0] + d[0], current[1] + d[1])
+            if next not in stepsdict and grid[next] != "#":
+                q.append(next)
+                stepsdict[next] = steps
+    return stepsdict
 
-q = []
-q.append(end)
-esteps = {}
-steps = 0
-esteps[end] = steps
-dirs = {"N":(0,-1), 
-        "E":(1,0),
-        "S":(0,1),
-        "W":(-1,0)}
-
-while q:
-    current = q.pop(0)
-    steps += 1
-    for d in dirs.values():
-        next = (current[0] + d[0], current[1] + d[1])
-        if next not in esteps and grid[next] != "#":
-            q.append(next)
-            esteps[next] = steps
+ssteps = getsteps(start, grid)
+esteps = getsteps(end, grid)
 
 # print(ssteps)
 # print(esteps)
 
 cheats = defaultdict(int)
 
+manhattencoords = {}
+for x in range(-21, 21):
+    for y in range(-21, 21):
+        cheatsteps = abs(x) + abs(y)
+        if cheatsteps <= 20:
+            manhattencoords[(x,y)] = cheatsteps
+
+
 for c in grid:
     if grid[c] != "#":
-        for x in range(-20, 20):
-            for y in range(-20, 20):
-                cheatsteps = abs(x) + abs(y)
-                if cheatsteps <= 20:
-                    cheatcoord = (c[0] + x, c[1] + y)
-                    try:
-                        if grid[cheatcoord] != "#":
-                            
-                            newsteps = ssteps[c]
-                            newsteps += esteps[cheatcoord]
-                            newsteps += cheatsteps
-                            difference = ssteps[end] - newsteps
+        for cheatdiff in manhattencoords:
+            cheatcoord = (c[0] + cheatdiff[0], cheatdiff[1] + c[1])
+            try:
+                if grid[cheatcoord] != "#":
 
-                            if difference >= 100:
-                                cheats[difference] += 1
-                    except:
-                        pass
+                    newsteps = ssteps[c]
+                    newsteps += manhattencoords[cheatdiff]
+                    newsteps += esteps[cheatcoord]
+
+                    difference = ssteps[end] - newsteps
+
+                    if difference >= 100:
+                        cheats[difference] += 1
+            except:
+                pass
 total = 0
 for k, v in sorted(cheats.items()):
-    print(v, k)
+    # print(v, k)
     total += v
-print(ssteps[end], esteps[start], total)
+print(total)
